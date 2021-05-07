91cbcc2911adc2ed5a7223790b74416a148b866e
Michał Pieczka <michalpieczka99@gmail.com>
2021-05-07 13:25:47 +0200

    router, nav

---
+++ resources/js/Spa.vue
@@ -0,0 +1,18 @@
+<>
+<template>
+    <div class="container">
+     Spa component<br/>
+     <navigation/>
+     <router-view></router-view>
+    </div>
+</template>
+<script>
+import Navigation from './components/Navigation.vue';
+export default {
+    components: {
+        Navigation
+    }
+};
+</script>
+
+</>
---
+++ resources/js/views/Login.vue
@@ -0,0 +1,10 @@
+<>
+<template>
+    <div>
+     <h1>Spa Login</h1>
+    </div>
+</template>
+<script>
+    export default { }
+</script>
+</>
